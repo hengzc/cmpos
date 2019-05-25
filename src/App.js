1bcaa31468ee061b378bd50ab476ae7126fa4ceb
Inzuliin <wisanu.kh@inet.co.th>
2019-05-25 16:15:57 +0700

    k

--- src/App.js
+++ src/App.js
@@ -19,7 +19,7 @@ const isLoggedIn = () =>{
   return localStorage.getItem(server.TOKEN_KEY) != null;
 }
 
-// Protected Route
+// Protected Route t
 const PrivateRoute = ({ component: Component, ...rest }) => (
   <Route
     {...rest}
